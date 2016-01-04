b86294ab8af1e1128ab981efe16763eecc32fa95
samvarankashyap <samvaran2best@gmail.com>
2016-01-04 01:19:45 +0000

    changed ping.py to command line parametered script

--- ping.py
+++ ping.py
@@ -1,10 +1,14 @@
 import re
 import subprocess
-website = "www.google.com"
-try:
-    ping = subprocess.Popen(["ping", "-n", "-c 5", website], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, error = ping.communicate()
-    print out.split("\n")[-3:-1]
-
-except subprocess.CalledProcessError:
-    print "Couldn't get a ping"
+import sys
+if len(sys.argv)==2:
+    website = sys.argv[1]
+    try:
+        ping = subprocess.Popen(["ping", "-n", "-c 5", website], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, error = ping.communicate()
+        print out.split("\n")[-3:-1]
+    except subprocess.CalledProcessError:
+        print "Couldn't get a ping"
+else:
+    #print sys.argv
+    print "invalid parameters"
