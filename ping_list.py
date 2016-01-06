80f11061efadc75a7e9837b57d377f5a235e4b68
samvarankashyap <samvaran2best@gmail.com>
2016-01-06 00:10:12 -0500

    updated ping list

--- ping_list.py
+++ ping_list.py
@@ -1,16 +1,26 @@
 import re
 import subprocess
 import sys
+import pdb
+pdb.set_trace()
 fd = open("ip_list.txt","r")
 ip_list = fd.readlines()
 for ip in ip_list:
     try:
+        print "current ip running is"
+        print ip
+        out_file = open("out_file.txt","a")
         ip = ip.strip("\n")
-        ping = subprocess.Popen(["ping", "-n", "-c 30", ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        ip = ip.split(",")
+        ipaddr = ip[0]
+        ping = subprocess.Popen(["ping", "-n", "-c 30", ipaddr], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, error = ping.communicate()
         out_list = out.split("\n")[-3:-1]
         out = '\n'.join(out_list)
-        out = "destination ip ping:"+ip+"\n"+out
+        #out = "\ndestination ip ping:"+ip+"\n"+out+"\n"
+        out = "destination ip::"+ip[0]+"::"+ip[1]+"::"+ip[2]+"\n"+out+"\n"
+        out_file.write(out)
         print out
+        out_file.close()
     except subprocess.CalledProcessError:
         print "Couldn't get a ping"
